53df954b2d6eb58b67165ea58c92bbbbd4a716e8
a2zsy <49357506+a2zsy@users.noreply.github.com>
2020-01-06 16:44:13 +0800

    Create actual_ladder_network.py

---
+++ work/actual_ladder_network.py
@@ -0,0 +1,175 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan  6 14:51:40 2020
+
+@author: 小源源
+"""
+from __future__ import print_function
+import pandas as pd
+import numpy as np
+import sys
+import sklearn
+col_names = ["duration","protocol_type","service","flag","src_bytes",
+    "dst_bytes","land","wrong_fragment","urgent","hot","num_failed_logins",
+    "logged_in","num_compromised","root_shell","su_attempted","num_root",
+    "num_file_creations","num_shells","num_access_files","num_outbound_cmds",
+    "is_host_login","is_guest_login","count","srv_count","serror_rate",
+    "srv_serror_rate","rerror_rate","srv_rerror_rate","same_srv_rate",
+    "diff_srv_rate","srv_diff_host_rate","dst_host_count","dst_host_srv_count",
+    "dst_host_same_srv_rate","dst_host_diff_srv_rate","dst_host_same_src_port_rate",
+    "dst_host_srv_diff_host_rate","dst_host_serror_rate","dst_host_srv_serror_rate",
+    "dst_host_rerror_rate","dst_host_srv_rerror_rate","label"]
+
+df = pd.read_csv(r"E:\study\ml\work\Network-Intrusion-Detection-master\KDDTrain+_2.csv", header=None, names=col_names)
+df_test = pd.read_csv(r"E:\study\ml\work\Network-Intrusion-Detection-master\KDDTest+_2.csv", header=None, names=col_names)
+ 
+#寻找train set当中的分类变量
+print('Training set:')
+for col_name in df.columns:
+    if df[col_name].dtypes == 'object' :
+        unique_cat = len(df[col_name].unique())
+        print("Feature '{col_name}' has {unique_cat} categories".format(col_name=col_name, unique_cat=unique_cat))
+print()
+print('Distribution of categories in service:')
+print(df['service'].value_counts().sort_values(ascending=False).head())
+# Test set
+print('Test set:')
+for col_name in df_test.columns:
+    if df_test[col_name].dtypes == 'object' :
+        unique_cat = len(df_test[col_name].unique())
+        print("Feature '{col_name}' has {unique_cat} categories".format(col_name=col_name, unique_cat=unique_cat))
+
+
+#开始进行onehot编码
+from sklearn.preprocessing import LabelEncoder,OneHotEncoder
+categorical_columns=['protocol_type', 'service', 'flag']
+categorical_columns=['protocol_type', 'service', 'flag'] 
+ # 将待分类变量转化为2维矩阵
+df_categorical_values = df[categorical_columns]
+testdf_categorical_values = df_test[categorical_columns]
+print(df_categorical_values.head())
+# protocol type
+unique_protocol=sorted(df.protocol_type.unique())
+string1 = 'Protocol_type_'
+unique_protocol2=[string1 + x for x in unique_protocol]
+# service
+unique_service=sorted(df.service.unique())
+string2 = 'service_'
+unique_service2=[string2 + x for x in unique_service]
+# flag
+unique_flag=sorted(df.flag.unique())
+string3 = 'flag_'
+unique_flag2=[string3 + x for x in unique_flag]
+# put together
+dumcols=unique_protocol2 + unique_service2 + unique_flag2
+print(dumcols)
+#训练集进行相同的操作
+unique_service_test=sorted(df_test.service.unique())
+unique_service2_test=[string2 + x for x in unique_service_test]
+testdumcols=unique_protocol2 + unique_service2_test + unique_flag2
+enc = OneHotEncoder()
+df_categorical_values_encenc = enc.fit_transform(df_categorical_values)
+df_cat_data = pd.DataFrame(df_categorical_values_encenc.toarray(),columns=dumcols)
+# test set
+testdf_categorical_values_encenc = enc.fit_transform(testdf_categorical_values)
+testdf_cat_data = pd.DataFrame(testdf_categorical_values_encenc.toarray(),columns=testdumcols)
+print(df_cat_data.head())
+#对测试和训练集中比对缺省的数据进行补充
+trainservice=df['service'].tolist()
+testservice= df_test['service'].tolist()
+difference=list(set(trainservice) - set(testservice))
+string = 'service_'
+difference=[string + x for x in difference]
+print(difference)
+
+for col in difference:
+    testdf_cat_data[col] = 0
+print(testdf_cat_data.shape)
+#补充新的数据矩阵
+newdf=df.join(df_cat_data)
+newdf.drop('flag', axis=1, inplace=True)
+newdf.drop('protocol_type', axis=1, inplace=True)
+newdf.drop('service', axis=1, inplace=True)
+# test data
+newdf_test=df_test.join(testdf_cat_data)
+newdf_test.drop('flag', axis=1, inplace=True)
+newdf_test.drop('protocol_type', axis=1, inplace=True)
+newdf_test.drop('service', axis=1, inplace=True)
+print(newdf.shape)
+print(newdf_test.shape)
+
+#根据攻击类型划分数据 0=normal, 1=DoS, 2=Probe, 3=R2L and 4=U2R.
+# 获取标签名
+labeldf=newdf['label']
+labeldf_test=newdf_test['label']
+# 改变标签名
+newlabeldf=labeldf.replace({ 'normal' : 0, 'neptune' : 1 ,'back': 1, 'land': 1, 'pod': 1, 'smurf': 1, 'teardrop': 1,'mailbomb': 1, 'apache2': 1, 'processtable': 1, 'udpstorm': 1, 'worm': 1,
+                           'ipsweep' : 2,'nmap' : 2,'portsweep' : 2,'satan' : 2,'mscan' : 2,'saint' : 2
+                           ,'ftp_write': 3,'guess_passwd': 3,'imap': 3,'multihop': 3,'phf': 3,'spy': 3,'warezclient': 3,'warezmaster': 3,'sendmail': 3,'named': 3,'snmpgetattack': 3,'snmpguess': 3,'xlock': 3,'xsnoop': 3,'httptunnel': 3,
+                           'buffer_overflow': 4,'loadmodule': 4,'perl': 4,'rootkit': 4,'ps': 4,'sqlattack': 4,'xterm': 4})
+newlabeldf_test=labeldf_test.replace({ 'normal' : 0, 'neptune' : 1 ,'back': 1, 'land': 1, 'pod': 1, 'smurf': 1, 'teardrop': 1,'mailbomb': 1, 'apache2': 1, 'processtable': 1, 'udpstorm': 1, 'worm': 1,
+                           'ipsweep' : 2,'nmap' : 2,'portsweep' : 2,'satan' : 2,'mscan' : 2,'saint' : 2
+                           ,'ftp_write': 3,'guess_passwd': 3,'imap': 3,'multihop': 3,'phf': 3,'spy': 3,'warezclient': 3,'warezmaster': 3,'sendmail': 3,'named': 3,'snmpgetattack': 3,'snmpguess': 3,'xlock': 3,'xsnoop': 3,'httptunnel': 3,
+                           'buffer_overflow': 4,'loadmodule': 4,'perl': 4,'rootkit': 4,'ps': 4,'sqlattack': 4,'xterm': 4})
+#替代标签
+newdf['label'] = newlabeldf
+newdf_test['label'] = newlabeldf_test
+#
+colNames=list(newdf_test)
+colNames_test=list(newdf_test)
+
+x_train = newdf.drop('label',1)
+y_train = newdf.label
+x_test = newdf_test.drop('label',1)
+y_test = newdf_test.label
+
+from sklearn import preprocessing
+scaler1 = preprocessing.StandardScaler().fit(x_train)
+x_train=scaler1.transform(x_train)
+print(x_train.std(axis=0))
+scaler2 = preprocessing.StandardScaler().fit(x_test)
+x_test=scaler2.transform(x_test)
+print(x_test.std(axis=0))
+
+
+from keras.datasets import mnist
+import keras
+import random
+from sklearn.metrics import accuracy_score
+import numpy as np
+from paper_ladder_net import get_ladder_network_fc
+
+# get the dataset
+inp_size = 122*1 # size of dataset 
+n_classes = 10
+
+
+
+x_train = x_train.astype('float32')
+x_test  = x_test.astype('float32')
+
+y_train = keras.utils.to_categorical(y_train, n_classes)
+y_test  = keras.utils.to_categorical(y_test,  n_classes)
+
+# only select 100 training samples 
+idxs_annot = range(x_train.shape[0])
+random.seed(0)
+idxs_annot = np.random.choice(x_train.shape[0], 9)
+
+x_train_unlabeled = x_train
+x_train_labeled   = x_train[idxs_annot]
+y_train_labeled   = y_train[idxs_annot]
+
+n_rep = x_train_unlabeled.shape[0] // x_train_labeled.shape[0]
+x_train_labeled_rep = np.concatenate([x_train_labeled]*n_rep)
+y_train_labeled_rep = np.concatenate([y_train_labeled]*n_rep)
+
+# initialize the model 
+model = get_ladder_network_fc(layer_sizes=[inp_size, 1000, 500, 250, 250, 250, n_classes])
+
+# train the model for 100 epochs
+for _ in range(9):
+    model.fit([x_train_labeled_rep, x_train_unlabeled], y_train_labeled_rep, epochs=1)
+    y_test_pr = model.test_model.predict(x_test, batch_size=100)
+    print("Test accuracy : %f" % accuracy_score(y_test.argmax(-1), y_test_pr.argmax(-1)))
+
